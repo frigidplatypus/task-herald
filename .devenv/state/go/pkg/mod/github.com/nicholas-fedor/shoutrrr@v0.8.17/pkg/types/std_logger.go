07248e3e910a30642b26f93bdceacdc127c290de
Justin Martin <jus10mar10@gmail.com>
2025-08-29 12:14:26 -0500

    chore: ignore .devenv for devenv state, keep direnv support

--- .devenv/state/go/pkg/mod/github.com/nicholas-fedor/shoutrrr@v0.8.17/pkg/types/std_logger.go
+++
@@ -1,8 +0,0 @@
-package types
-
-// StdLogger is an interface for outputting log information from services that are non-fatal.
-type StdLogger interface {
-	Print(args ...any)
-	Printf(format string, args ...any)
-	Println(args ...any)
-}
